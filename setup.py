ac704986eaa3fed105cdb7a020dbd33d57668507
Ulises <ulyses.sosa@gmail.com>
2025-04-11 11:04:52 +0200

    update git ignore add missing files to repo

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tool",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        'sentence-transformers',
+        'scikit-learn',
+        'numpy',
+        'dockerfile-parse',
+        'PyYAML',
+        'docker',
+        'checkov',
+        'dotenv'],
+    author="Ulises E. Sosa",
+    description="A tool for analyzing Docker configurations",
+    python_requires=">=3.6",
+)
